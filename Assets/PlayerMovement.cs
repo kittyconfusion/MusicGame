3eb9974c90dfeedb84fe0af6e4220d3c48a88c7d
kittyconfusion <65476906+kittyconfusion@users.noreply.github.com>
2022-06-11 21:21:54 -0500

    Project Files

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D body;
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        body.velocity = new Vector2(h * speed, v * speed);
+    }
+}
