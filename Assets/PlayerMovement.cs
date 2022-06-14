a117624febcf186dd7818f3bbb134858073f4c76
kittyconfusion <65476906+kittyconfusion@users.noreply.github.com>
2022-06-13 20:29:17 -0500

    Added Tiles
    removed Maggie

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,6 +11,10 @@ public class PlayerMovement : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        body.velocity = new Vector2(h * speed, v * speed);
+
+        body.velocity += new Vector2(h * speed, v * speed);
+        
+        
+
     }
 }
